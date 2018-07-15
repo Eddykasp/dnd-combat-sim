29b4439144a2fc989e4674833e049441489c256d
Eddykasp <maxkasperowski@gmail.com>
2018-07-15 18:08:47 +0800

    Add index.js as entry point to export all modules

---
+++ index.js
@@ -0,0 +1,11 @@
+const Combatant = require('./combatant');
+const Dice = require('./dice_roller');
+const Party = require('./party');
+const Combat = require('./combat');
+
+module.exports = {
+  Combatant: Combatant,
+  Dice: Dice,
+  Party: Party,
+  Combat: Combat
+}
