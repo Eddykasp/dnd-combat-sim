9c5bfa21f9161561f6425e2947ce3a01a530356f
Eddykasp <maxkasperowski@gmail.com>
2018-07-15 13:34:37 +0800

    Add combatant methods and tests
    Initiative, attack, damage rolls
    Checking for hits
    Taking damage
    Checking for death

--- combatant.js
+++ combatant.js
@@ -29,6 +29,34 @@ module.exports = function(hp, ac, initiative, atk, dmg, dmg_dice, dmg_bonus){
   this.dmg_dice = dmg_dice;
   this.dmg_bonus = dmg_bonus;
   this.rollInitiative = function(){
-    return dice(20, this.initiative)
+    return dice(20, this.initiative);
+  };
+  this.attackRoll = function(){
+    return dice(20, atk);
+  };
+  this.damageRoll = function(){
+    let sum = 0;
+    for(let i = 0; i < this.dmg_dice; i++){
+      sum += dice(this.dmg, 0);
+    }
+    return sum + this.dmg_bonus;
+  };
+  this.isHit = function(attackRoll){
+    if(attackRoll >= this.ac){
+      return true;
+    }
+  };
+  this.takeDamage = function(damageRoll){
+    this.hp -= damageRoll;
+    if (this.hp < 0){
+      this.hp = 0;
+    }
+  };
+  this.isDead = function(){
+    if (this.hp <= 0) {
+      return true;
+    } else {
+      return false;
+    }
   };
 };
--- test/combatant.test.js
+++ test/combatant.test.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const Combatant = require('../combatant.js')
 
-test('Test combatant constructor', t => {
+test('Combatant constructor', t => {
   t.plan(7);
   c = new Combatant(10, 2, 2, 3, 6, 1, 1);
   t.equal(c.hp, 10, 'hp');
@@ -11,4 +11,76 @@ test('Test combatant constructor', t => {
   t.equal(c.dmg, 6, 'dmg');
   t.equal(c.dmg_dice, 1, 'dmg_dice');
   t.equal(c.dmg_bonus, 1, 'dmg_bonus');
+});
+
+test('Combatant initiative roll', t => {
+  c = new Combatant(10, 2, 2, 3, 6, 1, 1);
+  i = c.rollInitiative();
+  if(i <= 2 || i > 22){
+    t.fail('Initiative roll out of valid range');
+  } else {
+    t.pass('Initiative roll within valid range');
+  }
+  t.end();
+});
+
+test('Combatant attack roll', t => {
+  c = new Combatant(10, 2, 2, 3, 6, 1, 1);
+  a = c.attackRoll();
+  if(a <= 2 || a > 22){
+    t.fail('Attack roll out of valid range');
+  } else {
+    t.pass('Attack roll within valid range');
+  }
+  t.end();
+});
+
+test('Combatant damage roll single die', t => {
+  c = new Combatant(10, 2, 2, 3, 6, 1, 1);
+  d = c.damageRoll();
+  if(d <= 1 || d > 7){
+    t.fail('Damage roll out of valid range');
+  } else {
+    t.pass('Damage roll within valid range');
+  }
+  t.end();
+});
+
+test('Combatant damage roll multiple dice', t => {
+  c = new Combatant(10, 2, 2, 3, 6, 3, 1);
+  d = c.damageRoll();
+  if(d <= 4 || d > 19){
+    t.fail('Damage roll out of valid range');
+  } else {
+    t.pass('Damage roll within valid range');
+  }
+  t.end();
+});
+
+test('Combatant is hit', t => {
+  t.plan(3);
+  c = new Combatant(10, 12, 2, 3, 6, 3, 1);
+  hit = c.isHit(10);
+  t.false(hit, 'atk=10, AC=12');
+  hit = c.isHit(12);
+  t.true(hit, 'atk=12, AC=12');
+  hit = c.isHit(14);
+  t.true(hit, 'atk=14, AC=12');
+});
+
+test('Combatant is dealt damage', t => {
+  t.plan(2);
+  c = new Combatant(10, 12, 2, 3, 6, 3, 1);
+  c.takeDamage(5);
+  t.equal(c.hp, 5, 'hp: 10 -> 5 (5 damage)');
+  c.takeDamage(6);
+  t.equal(c.hp, 0, 'hp: 5 ->  0 (6 damage)');
+});
+
+test('Combatant is dead/unconscious', t => {
+  t.plan(2);
+  c = new Combatant(10, 12, 2, 3, 6, 3, 1);
+  t.false(c.isDead(), 'hp: 10 (alive)');
+  c.takeDamage(10);
+  t.true(c.isDead(), 'hp: 0 (dead)');
 });
