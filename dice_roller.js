5fa960e72ff2a0ca97a5c23ce99b401261409463
Eddykasp <maxkasperowski@gmail.com>
2018-07-15 11:38:52 +0800

    Add dice rolling module and tests

---
+++ dice_roller.js
@@ -0,0 +1,4 @@
+module.exports = function(dice_type, modifier){
+  roll = Math.floor(Math.random() * dice_type + 1);
+  return roll + modifier;
+}
---
+++ test/dice_roller.test.js
@@ -0,0 +1,122 @@
+const test = require('tape');
+const dice = require('../dice_roller');
+
+test('d4 is in range', t => {
+  roll = dice(4, 0);
+  if (roll <= 0 || roll > 4) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d6 is in range', t => {
+  roll = dice(6, 0);
+  if (roll <= 0 || roll > 6) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d8 is in range', t => {
+  roll = dice(8, 0);
+  if (roll <= 0 || roll > 8) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d10 is in range', t => {
+  roll = dice(10, 0);
+  if (roll <= 0 || roll > 10) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d12 is in range', t => {
+  roll = dice(12, 0);
+  if (roll <= 0 || roll > 12) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d20 is in range', t => {
+  roll = dice(20, 0);
+  if (roll <= 0 || roll > 20) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d4+2 is in range', t => {
+  roll = dice(4, 0+2);
+  if (roll <= 0+2 || roll > 4+2) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d6+2 is in range', t => {
+  roll = dice(6, 0+2);
+  if (roll <= 0+2 || roll > 6+2) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d8+2 is in range', t => {
+  roll = dice(8, 0+2);
+  if (roll <= 0+2 || roll > 8+2) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d10+2 is in range', t => {
+  roll = dice(10, 0+2);
+  if (roll <= 0+2 || roll > 10+2) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d12+2 is in range', t => {
+  roll = dice(12, 0+2);
+  if (roll <= 0+2 || roll > 12+2) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
+
+test('d20+2 is in range', t => {
+  roll = dice(20, 0+2);
+  if (roll <= 0+2 || roll > 20+2) {
+    t.fail('Dice roll out of expected range');
+  } else {
+    t.pass('Dice roll within range');
+  }
+  t.end()
+});
