acac90692d49d831fe3b87945e17833fbfa3b03e
Eddykasp <maxkasperowski@gmail.com>
2018-07-19 11:38:12 +0800

    Add new party combat strategy
    highest_dmg targets the creature with the highest potential damage per turn

--- party.js
+++ party.js
@@ -6,6 +6,21 @@ module.exports = function(){
     },
     highest_hp: function(combatantA, combatantB){
       return combatantB.hp - combatantA.hp;
+    },
+    highest_dmg: function(combatantA, combatantB){
+      let dmgA = 0;
+      combatantA.attacks.forEach(function(element) {
+        let dmg = element.dice_type * element.dice_number + element.dmg_bonus;
+        dmgA += dmg;
+      });
+
+      let dmgB = 0;
+      combatantB.attacks.forEach(function(element) {
+        let dmg = element.dice_type * element.dice_number + element.dmg_bonus;
+        dmgB += dmg;
+      });
+
+      return dmgB - dmgA;
     }
   };
   this.combatStrategy = this.combatStrategies.lowest_hp;
