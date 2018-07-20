dcc3b2161455b7144f00462037f88de9bd10adde
Eddykasp <maxkasperowski@gmail.com>
2018-07-20 13:02:26 +0800

    Expost Buff constructor

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const dice = require('./dice_roller');
 const Party = require('./party');
 const Combat = require('./combat');
 const Attack = require('./attack');
+const Buff = require('./buff');
 
 module.exports = {
   Combatant: Combatant,
   dice: dice,
   Party: Party,
   Combat: Combat,
-  Attack: Attack
+  Attack: Attack,
+  Buff: Buff
 }
