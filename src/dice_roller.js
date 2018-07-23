180fcddb7ce33a28b76cba39b9e821d4077db4d0
Eddykasp <maxkasperowski@gmail.com>
2018-07-23 12:57:11 +0800

    Fix dice rolling bug

--- src/dice_roller.js
+++ src/dice_roller.js
@@ -1,14 +1,18 @@
 module.exports = function(dice_type, modifier, options){
   let roll = 0;
-  if (!!options.advantage){
-    let roll1 = Math.floor(Math.random() * dice_type + 1);
-    let roll2 = Math.floor(Math.random() * dice_type + 1);
-    if (options.advantage < 0){
-      roll = roll1 < roll2 ? roll1 : roll2;
+  if (!!options){
+    if (!!options.advantage){
+      let roll1 = Math.floor(Math.random() * dice_type + 1);
+      let roll2 = Math.floor(Math.random() * dice_type + 1);
+      if (options.advantage < 0){
+        roll = roll1 < roll2 ? roll1 : roll2;
+      }
+      if (options.advantage > 0){
+        roll = roll1 > roll2 ? roll2 : roll1;
+      }
     }
-    if (options.advantage > 0){
-      roll = roll1 > roll2 ? roll2 : roll1;
-    }
+  } else {
+    roll = Math.floor(Math.random() * dice_type + 1);
   }
   return roll + modifier;
 };
