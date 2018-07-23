d9b35be40c174fc2cc5ccf014cc9ce021d5c5d59
Eddykasp <maxkasperowski@gmail.com>
2018-07-23 11:22:21 +0800

    Add advantage options to dice rolling

--- src/dice_roller.js
+++ src/dice_roller.js
@@ -1,4 +1,14 @@
-module.exports = function(dice_type, modifier){
-  let roll = Math.floor(Math.random() * dice_type + 1);
+module.exports = function(dice_type, modifier, options){
+  let roll = 0;
+  if (!!options.advantage){
+    let roll1 = Math.floor(Math.random() * dice_type + 1);
+    let roll2 = Math.floor(Math.random() * dice_type + 1);
+    if (options.advantage < 0){
+      roll = roll1 < roll2 ? roll1 : roll2;
+    }
+    if (options.advantage > 0){
+      roll = roll1 > roll2 ? roll2 : roll1;
+    }
+  }
   return roll + modifier;
 };
